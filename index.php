21a11e44b10d9926fbb8d79594b0f80c57823109
Yago Ananias <yago.crazy@hotmail.com>
2018-06-30 06:47:20 -0300

    Configurando PageAdmin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 //Namespaces
 use \Slim\Slim;
 use \yagoananias\Page;
+use \yagoananias\PageAdmin;
 //Nova Aplicação(Rotas)
 $app = new Slim();
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {
     
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin/', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
